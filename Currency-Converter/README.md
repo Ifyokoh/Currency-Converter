55b481b1b322abde8e8837714a975b31830be90b
Ify <odibest1893@gmail.com>
2018-06-30 12:30:04 +0100

    modified pages

---
+++ Currency-Converter/README.md
@@ -0,0 +1,2 @@
+# Currency-Converter
+A web app that converts currency from one exchange rate to another
---
+++ Currency-Converter/sw.js
@@ -0,0 +1,62 @@
+var cacheName = 'Currency Converter';
+var filesToCache = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/script.js',
+    './bg.jpg',
+    'https://free.currencyconverterapi.com/api/v5/currencies'
+];
+
+self.addEventListener('install', event => {
+    console.log('[ServiceWorker] Install');
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(function(cache){
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+        .then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cName) {
+                    if(cName !== cacheName){
+                        return caches.delete(cName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          }
+          var fetchRequest = event.request.clone();
+          return fetch(fetchRequest).then(
+            function(response) {
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+              var responseToCache = response.clone();
+              caches.open(cacheName)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+              return response;
+            }
+          );
+        })
+      );
+  });
+
